246b3762c60385454eedfe5366d1c9bc95319e0d
HuyNguyen777 <huynguyen6498@gmail.com>
2025-02-25 15:57:38 +0700

    Initial commit

--- data/user-data.js
+++ data/user-data.js
@@ -1,13 +1,13 @@
 export const userData = {
-  githubUser: 'said7388',
-  devUsername: "said7388",
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  githubUser: 'HuyNguyen777',
+  devUsername: "HuyNguyen777",
+  github: 'https://github.com/HuyNguyen777',
+  facebook: 'https://www.facebook.com/huyng67/',
+  linkedIn: '#',
+  twitter: '#',
+  stackOverflow: '#',
+  leetcode: "https://leetcode.com/u/duchuy0607/",
+  resume: "https://drive.google.com/file/d/1OFNYhibleQa4KUjdXHrruIuqbHSkQsZh/view?usp=sharing",
   skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
-  timezone: '+6'
+  timezone: '+7'
 };
